4e0a408f3cc5cde73743c61ddde552b34a0fd52f
Seiji Sawayanagi <mostlyfine@gmail.com>
2011-09-30 10:57:01 +0900

    すでに.sshがある場合は追加できるように

--- 06_ssh.sh
+++ 06_ssh.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir ~/.ssh
+mkdir -p ~/.ssh
 
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAsblJsnvO8uSp+aBuDj4FZSrBZm/nwEWYy0OxKOpuavKItx+mmAT0+JOB2EGmI2XIacTYU2hU6Hc/KcxFJExSIcQ+bZq2dZaNuR54ShLMLUY6bWxPm94sCGJ5MKuSk+DyfDlUKhIBqYYGPhd7yRnOvxndpHrYmVsDP3IFR/wdlq0/6+0SVQXwiSMbETW78UcXWvesmzB8UjT+gw+jvBmlq/2YisGue4Rug26qAqyTB0wFqzKehig25YFvS3TqnWJjJ9XOdRqszQCxa0qniiUiIdhPKHzSzxerM427o/FU+2Mr7oJ8cbU+ijiuHAd9kT2VqHY7RepnPJ4LYWb4DRWhDw== sawa@ubuntu" > ~/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAsblJsnvO8uSp+aBuDj4FZSrBZm/nwEWYy0OxKOpuavKItx+mmAT0+JOB2EGmI2XIacTYU2hU6Hc/KcxFJExSIcQ+bZq2dZaNuR54ShLMLUY6bWxPm94sCGJ5MKuSk+DyfDlUKhIBqYYGPhd7yRnOvxndpHrYmVsDP3IFR/wdlq0/6+0SVQXwiSMbETW78UcXWvesmzB8UjT+gw+jvBmlq/2YisGue4Rug26qAqyTB0wFqzKehig25YFvS3TqnWJjJ9XOdRqszQCxa0qniiUiIdhPKHzSzxerM427o/FU+2Mr7oJ8cbU+ijiuHAd9kT2VqHY7RepnPJ4LYWb4DRWhDw== sawa@ubuntu" >> ~/.ssh/authorized_keys
 
 
 chmod 700 ~/.ssh
