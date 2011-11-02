83467b802eeea3875d6913395729b2d113169b7c
Seiji Sawayanagi <mostlyfine@gmail.com>
2011-11-02 22:07:02 +0900

    compile success

--- 20_varnish.sh
+++ 20_varnish.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-#rpm --nosignature -i http://repo.varnish-cache.org/redhat/varnish-3.0/el5/noarch/varnish-release-3.0-1.noarch.rpm
-#yum install -y logrotate libgomp gcc cpp binutils kernel-headers glibc-headers glibc-devel expect
+# see http://sabo.posterous.com/varnish-2
+
+yum erase libtool
+rpm -i http://dl.atrpms.net/el5-i386/atrpms/testing/libtool-2.2.6-15.5.el5.1.i386.rpm
 
 yum install -y pcre-devel expect
-yum install -y ncurses-devel automake autoconf libtool libxslt groff pkgconfig
+yum install -y ncurses-devel automake autoconf libxslt groff pkgconfig
 
 wget http://repo.varnish-cache.org/source/varnish-3.0.2.tar.gz
 tar xvzf varnish-3.0.2.tar.gz
@@ -13,16 +15,16 @@ cd varnish-3.0.2
 ./configure --prefix /usr/local/varnish --enable-debugging-symbols
 make && make install
 
-#cp redhat/varnish.initrc /etc/init.d/varnish 
-#cp redhat/varnish.logrotate /etc/logrotate.d/varnish 
-#cp redhat/varnish.sysconfig /etc/sysconfig/varnish 
-#cp redhat/varnishlog.initrc /etc/init.d/varnishlog 
-#cp redhat/varnishncsa.initrc /etc/init.d/varnishncsa 
+#cp redhat/varnish.initrc /etc/init.d/varnish
+#cp redhat/varnish.logrotate /etc/logrotate.d/varnish
+#cp redhat/varnish.sysconfig /etc/sysconfig/varnish
+#cp redhat/varnishlog.initrc /etc/init.d/varnishlog
+#cp redhat/varnishncsa.initrc /etc/init.d/varnishncsa
 #cp etc/default.vcl /usr/local/varnish/etc/default.vcl
 #
-#chkconfig -add varnish 
-#chkconfig -add varnishncsa 
-#chkconfig -add varnishlog 
-#chkconfig -level 2345 varnish on 
-#chkconfig -level 2345 varnishncsa on 
+#chkconfig -add varnish
+#chkconfig -add varnishncsa
+#chkconfig -add varnishlog
+#chkconfig -level 2345 varnish on
+#chkconfig -level 2345 varnishncsa on
 #chkconfig -level 2345 varnishlog on
